cb12ac8dc2350d8d3c6824eb2e92e52281275022
Hitansh Doshi <hkd159@gmail.com>
2023-07-21 00:12:04 +0530

    added listener endpoint

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,23 @@
+import { StorageProvider } from "#storage/storageProvider";
+import { asyncPlaceholders, logger } from "#util";
+
+const storage = new StorageProvider(); // eslint-disable-line no-unused-vars
+// as storage setup takes time and async can't be used
+await asyncPlaceholders("sleep", 500);
+
 function home(req, res) {
   res.json({ res: "Server Working" });
 }
 
-export default { home };
+function listener(req, res) {
+  try {
+    const log = req.body;
+    storage.write(log);
+    res.json({ data: [], meta: { message: "log recorded" } });
+  } catch (err) {
+    res.json({ data: [], meta: { message: err.message } });
+    logger.error("Error while reciving log: ", err);
+  }
+}
+
+export default { home, listener };
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", indexController.home);
+router.post("/listener", indexController.listener);
 
 export default router;
